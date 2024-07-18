93879aa405fe47af436906bac0584dbd1537af78
Rishav <51906186+rishavmahapatra@users.noreply.github.com>
2024-07-19 01:57:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Bar from './Bar';
+import Home from './Home';
+
+function App() {
+  return (
+    <>
+      
+      <Bar/>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
+
+
